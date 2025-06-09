c33df4563709593fab492a2c37465f378b30afa5
Dylan Fletcher <guitarherody@gmail.com>
2025-06-09 14:57:47 +0200

    Fix backend build script to change to correct directory

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Change to the backend directory (where this script is located)
+cd "$SCRIPT_DIR"
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
